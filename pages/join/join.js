026fc884606a89023263d2beb9c4f28de74fbb14
269043389 <269043389@qq.com>
2017-05-18 00:54:10 +0800

    add

---
+++ pages/join/join.js
@@ -0,0 +1,26 @@
+var api = require('../../api/api.js')
+
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+  },
+  onReady: function () {
+    wx.setNavigationBarTitle({
+      title: '加入我们'
+    })
+  },
+  onLoad: function () {
+    var that = this;
+    
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    });
+  }
+})
