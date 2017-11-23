38ff4a5cfa8fceeba4468225ad31c682ad4b381b
269043389 <269043389@qq.com>
2017-11-23 23:29:21 +0800

    edit

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,7 +15,16 @@ Page({
     requests: {
       // 查看的详情实体
       Model: {},
-    }
+    },
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
   },
   onShareAppMessage: function () {
       return {
