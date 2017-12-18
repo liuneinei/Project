9d8683b91a066ce37a032c2efdbfad6df61077bb
liuneinei <liuneinei@163.com>
2017-12-18 18:13:33 +0800

    2017-12-18

--- pages/news/news.js
+++ pages/news/news.js
@@ -7,151 +7,7 @@ Page({
   data: {
     // 课程
     coursesdata:{
-      list: [
-        {
-          id: 1,
-          title: 'DOJ watchdog reveals how ex-Mueller agents anti-Trump texts came to light',
-          img: 'http://a57.foxnews.com/hp.foxnews.com/images/2017/12/1024/576/d680001fcc19fbd356eefe7a8fc72fb0.jpg',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        },
-        {
-          id: 1,
-          title: 'Three arrested after Connecticut man is thrown 45 feet off bridge, police say',
-          img: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/us/2017/12/14/three-arrested-after-connecticut-man-is-thrown-45-feet-off-bridge-police-say/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1513234477158.jpg?ve=1&tl=1&text=big-top-image',
-          tags: [
-            {
-              name: '标签1',
-            },
-            {
-              name: '标签2',
-            }
-          ],
-        }
-      ],
+      list: [],
     },
     // 请求参数
     req: {
@@ -166,16 +22,18 @@ Page({
     },
     scroll:{
       height:300,
+    },
+    // 扩展属性
+    exp: {
+      host: '',//域名
     }
   },
   onLoad: function () {
     var that = this;
-    var _sysinfo = wx.getSystemInfoSync();
-    var _scroll = that.data.scroll;
-    _scroll.height = _sysinfo.windowHeight;
-    that.setData({
-      scroll: _scroll
-    })
+    
+    // 初始化扩展属性
+    InitExp(that);
+
     // 课程列表
     getcourse(that, function (res) {
       //回调函数
@@ -224,10 +82,34 @@ Page({
 })
 
 // Begin : 扩展方法
+
+function InitExp(that) {
+  var _exp = that.data.exp;
+
+  // 域名赋值
+  if (_exp.host.indexOf('http://') < 0) {
+    _exp.host = api.iQiniu;
+  }
+
+  // 获取屏幕高度
+  var _sysinfo = wx.getSystemInfoSync();
+  var _scroll = that.data.scroll;
+  _scroll.height = _sysinfo.windowHeight;
+
+  // 重新赋值
+  that.setData({
+    exp: _exp,
+    scroll: _scroll
+  });
+}
+
 /*
 * 加载列表
 */
 function getcourse(that, fb){
+  // data - 列表
+  var _coursesdata = that.data.coursesdata;
+  // data - 参数
   var _req = that.data.req;
   // 获取讲师列表
   api.wxRequest({
@@ -238,7 +120,41 @@ function getcourse(that, fb){
       page: _req.page,
     },
     success:function(res){
-      console.log(res);
+      // api - 接口返回状态
+      if (res.statusCode == 200){
+        var _data = res.data;
+        // api - 接口返回数据状态
+        if (_data.status == 0 && _data.errorcode ==0){
+          // api - 接口返回数据
+          _data = _data.data;
+          // data - 显示的记录数
+          _req.maxlen = _data.per_page;
+          // data - 是否已经加载完
+          _req.over = functions.pageover(_data.current_page, _data.per_page, _data.total);
+
+          var $arr = [];
+          [].forEach.call(_data.data,function(item,index){
+            var $_arr ={
+              id: item.id,
+              title: item.name,
+              img: item.face_img,
+              tags: item.service
+            };
+            $arr.push($_arr);
+          });
+
+          if (_req.page <= 1){
+            _coursesdata.list = $arr;
+          }else{
+            _coursesdata.list = _coursesdata.list.concat($arr);
+          }
+
+          that.setData({
+            coursesdata: _coursesdata,
+            req: _req,
+          })
+        }
+      }
     },
     fail:function(res){
       console.log(res);
